8c3fb998e513b70ba8c10efec9e8b679dc8ee381
VitalikVoicix <voicix1502@gmail.com>
2020-09-22 19:07:16 +0300

    Updates

---
+++ precache-manifest.f0020af9f4e53df60ae002af162a4ccf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03a25406137d79355778c5954109469",
+    "url": "/homework19/index.html"
+  },
+  {
+    "revision": "0b9bc013e6a5d3863ae0",
+    "url": "/homework19/static/css/main.b944cd74.chunk.css"
+  },
+  {
+    "revision": "0acfda4517ffb3533dba",
+    "url": "/homework19/static/js/2.9e906507.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homework19/static/js/2.9e906507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9bc013e6a5d3863ae0",
+    "url": "/homework19/static/js/main.f5cde8fd.chunk.js"
+  },
+  {
+    "revision": "c1997f84c4baee2fb2a4",
+    "url": "/homework19/static/js/runtime-main.cbe2dea2.js"
+  },
+  {
+    "revision": "d433ddc41405da15bed8778959cfbe13",
+    "url": "/homework19/static/media/chr.d433ddc4.jpg"
+  },
+  {
+    "revision": "e71c4cf2b24cb4653ccf36f5662c996b",
+    "url": "/homework19/static/media/hilux.e71c4cf2.jpg"
+  },
+  {
+    "revision": "2dc3737d63ffca9643914f29409ef008",
+    "url": "/homework19/static/media/proace.2dc3737d.jpg"
+  },
+  {
+    "revision": "29755b30739c1dc457582e9ac6e8924b",
+    "url": "/homework19/static/media/toyota.29755b30.jpg"
+  },
+  {
+    "revision": "a436eb93d878d87fcdd9a01c9a5328f7",
+    "url": "/homework19/static/media/toyota1.a436eb93.jpg"
+  },
+  {
+    "revision": "940dbb22c8294284573be856facff87b",
+    "url": "/homework19/static/media/toyota2.940dbb22.jpg"
+  },
+  {
+    "revision": "ca74ecae20dbdbe013e18ce55aa082c4",
+    "url": "/homework19/static/media/toyota3.ca74ecae.jpg"
+  }
+]);
